8c55840c6943677160509090ff73866bc92e3cea
resascreative <you@example.com>
2020-02-06 19:02:53 -0800

    test

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//STEP 1
+// var some_Month;
+// function the Month();
+// var current-Month;
+// var summer_Month;
+// var myLibrary-Function
+
+//STEP 2
+// 144
+// "Hi my name is"
+// true
+// null
+
+//STEP 3
